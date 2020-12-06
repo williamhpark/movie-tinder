2f9c9ce08b2f5bcd096c8870dc9f5a1aad59b468
William Park <william.h.park@gmail.com>
2020-12-06 14:25:27 -0800

    Added header to genres list

--- client/src/components/GenreSelect/GenreSelect.js
+++ client/src/components/GenreSelect/GenreSelect.js
@@ -2,11 +2,14 @@ import React from "react";
 
 const GenreSelect = (props) => {
   return (
-    <ol>
-      {props.genres.map((genre) => {
-        return <li key={genre}>{genre}</li>;
-      })}
-    </ol>
+    <div>
+      <h1>Genres</h1>
+      <ol>
+        {props.genres.map((genre) => {
+          return <li key={genre}>{genre}</li>;
+        })}
+      </ol>
+    </div>
   );
 };
 
